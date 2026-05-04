654730c1132256ba09d2268bad572091cb4e82e9
Jeanchar05 <rodriguesmirian274@gmail.com>
2026-05-03 22:41:58 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -83,6 +83,8 @@ app.post("/aprovar", async (req, res) => {
 });
 
 // ==========================
-app.listen(3000, () => {
-  console.log("API rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
