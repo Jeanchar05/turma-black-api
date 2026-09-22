ba2d6cae76b8d355021db34dd4247bf8f676b42c
Jeanchar05 <rodriguesmirian274@gmail.com>
2026-09-22 20:56:07 -0300

    Carrega camada final V12 nas páginas do aluno

--- public/page-navigation.js
+++ public/page-navigation.js
@@ -95,6 +95,8 @@
       loadScript("/roulette-esportiva-affiliate-v10.js?v=20260922-1", "roulette-esportiva-affiliate-v10-js");
       loadScript("/roulette-card-art-v11.js?v=20260922-1", "roulette-card-art-v11-js");
     }
+    loadStyle("/final-fixes-v12.css?v=20260922-1", "final-fixes-v12-css");
+    loadScript("/final-fixes-v12.js?v=20260922-1", "final-fixes-v12-js");
   }
   loadEvolutionV6();
 })();
